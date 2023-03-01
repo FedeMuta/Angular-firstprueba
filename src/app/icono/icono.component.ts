5a976185d0b1af92741036cc2e1e154fcd449ed1
Fede Muta <fedemuta@gmail.com>
2023-03-01 17:55:58 -0300

    icono sin .html

--- src/app/icono/icono.component.ts
+++ src/app/icono/icono.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-icono',
-  templateUrl: './icono.component.html',
+  template: '<a [href]="link" target="_blank"><img [src]="imagen" [alt]="alt" class="new" [title]="alt" /></a>',
   styleUrls: ['./icono.component.css'],
 })
 export class IconoComponent {
