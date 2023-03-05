24ecf2ea7d15f066b3089ab2d027d17484be25e2
Fede Muta <fedemuta@gmail.com>
2023-03-05 15:26:17 -0300

    proyectos, proyectcard

--- src/app/proyectos/proyectcard/proyectcard.component.ts
+++ src/app/proyectos/proyectcard/proyectcard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-proyectcard',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./proyectcard.component.css']
 })
 export class ProyectcardComponent {
+
+  @Input() name="";
+  @Input() descripcion="";
+  @Input() link="";
+  @Input() img="";
 
 }
