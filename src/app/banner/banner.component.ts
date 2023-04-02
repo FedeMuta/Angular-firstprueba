d8651bd96a9fd30a33fd3d9fa4981a2868ba2967
Fede Muta <fedemuta@gmail.com>
2023-04-02 16:34:50 -0300

    Banner con CSS, HTML. Rellax js solo en fondo

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Rellax from 'rellax';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
 })
-export class BannerComponent {
-
+export class BannerComponent implements OnInit {
+  ngOnInit(): void {
+    new Rellax('.rellax-background', {
+      speed: -4,
+      center: true,
+    });
+  }
 }
